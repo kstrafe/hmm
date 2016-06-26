11efe04d30c04dac6479a179cd53c26dc332939d
oaramsda <olea.ramsdal@gmail.com>
2016-06-26 19:20:22 +0200

    Structuring and adding partially functioning highlighing of circles

--- js_graphics/circular_button.js
+++ js_graphics/circular_button.js
@@ -4,13 +4,7 @@ var ctx = canvas.getContext('2d');
 canvasHeight = 720;
 canvasWidth = 1280;
 
-gradient = ctx.createRadialGradient(canvasWidth/2, canvasHeight/2, 5, canvasWidth/2, canvasHeight/2, 300);
-gradient.addColorStop(0, '#000028');
-gradient.addColorStop(1, '#080808');
-ctx.fillStyle = gradient;
-ctx.fillRect(0,0,canvasWidth, canvasHeight);
-
-function renderButton(x, y, r, text) {
+function renderButton(x, y, r, text, highlighted=0) {
 	ctx.beginPath();
 	ctx.arc(x, y, r, 0, 2 * Math.PI);
 	var p = ctx.getImageData(x, y, 1, 1).data; 
@@ -19,8 +13,12 @@ function renderButton(x, y, r, text) {
 	ctx.fill();
 	ctx.lineWidth = 3;
 	ctx.strokeStyle = '#69B00C';
-	ctx.shadowBlur = 10;
-	ctx.shadowColor = '#6EB80D';
+  if (highlighted) {
+    ctx.shadowBlur = 10;
+    ctx.shadowColor = '#6EB80D';
+
+  }
+	
 	ctx.stroke();
 
 	ctx.font = "18px Calibri";
@@ -56,16 +54,66 @@ function rgbToHex(r, g, b) {
     return ((r << 16) | (g << 8) | b).toString(16);
 }
 
-ctx.beginPath();
-ctx.moveTo(canvasWidth/2, canvasHeight/2);
-ctx.quadraticCurveTo(canvasHeight/2 + 50,canvasHeight/2 + 50,canvasWidth/2 + 300,canvasHeight/2+ 200);
-ctx.lineWidth = 2;
-ctx.strokeStyle = '#6EB80D';
-ctx.stroke();
+function renderCanvas(circles) {
+  ctx.clearRect(0,0, canvas.width, canvas.height);
+  gradient = ctx.createRadialGradient(canvasWidth/2, canvasHeight/2, 5, canvasWidth/2, canvasHeight/2, 300);
+  gradient.addColorStop(0, '#000028');
+  gradient.addColorStop(1, '#080808');
+  ctx.fillStyle = gradient;
+  ctx.fillRect(0,0,canvasWidth, canvasHeight);
+  
+  renderTestLine();
+
+  for (var i=0; i < circles.length; i++) {
+    renderButton(circles[i].x, circles[i].y, circles[i].r, circles[i].text, circles[i].hl);
+  }
+}
 
 
-renderButton(canvasWidth/2, canvasHeight/2, 50, 'Natural numbers');
-renderButton(canvasWidth/2 + 300, canvasHeight/2+ 200, 40, 'Complex numbers');
+function renderTestLine() {
+  ctx.beginPath();
+  ctx.moveTo(canvasWidth/2, canvasHeight/2);
+  ctx.quadraticCurveTo(canvasHeight/2 + 50,canvasHeight/2 + 50,canvasWidth/2 + 300,canvasHeight/2+ 200);
+  ctx.lineWidth = 2;
+  ctx.strokeStyle = '#6EB80D';
+  ctx.stroke();
+}
+
+
+
+var circle1 = {x:canvasWidth/2, y:canvasHeight/2 , r:50, text:'Natural numbers', hl:false}
+var circle2 = {x:canvasWidth/2 + 300, y:canvasHeight/2+ 200, r:40, text:'Complex numbers', hl:false}
+var circles = [circle1, circle2];
+
+renderCanvas(circles)
+//console.log();
+
+
+function getMousePos(canvas, evt) {
+  var rect = canvas.getBoundingClientRect();
+  //console.log(rect)
+  return {
+    x: evt.clientX - rect.left,
+    y: evt.clientY - rect.top
+  };
+}
+
+canvas.addEventListener('mousemove', function(evt) {
+  //renderCanvas(circles)
+  var mousePos = getMousePos(canvas, evt);
+  for (var i=0; i < circles.length; i++) {
+    rMouseCenter = Math.pow((mousePos.x-circles[i].x),2) + Math.pow((mousePos.y-circles[i].y),2);
+    console.log(rMouseCenter)
+    if (rMouseCenter < Math.pow(circles[i].r,2)) {
+      circles[i].hl = true;
+      renderCanvas(circles);
+    } else {
+      circles[i].hl = false;
+      renderCanvas(circles);    
+    }
+    
+  }
+}, false);
 
 
 
