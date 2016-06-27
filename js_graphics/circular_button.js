4c0bc37f80b676a1551fd170371d479533803a42
oaramsda <olea.ramsdal@gmail.com>
2016-06-27 17:43:30 +0200

    Fix highlighting on hover and add sound effect for hover

--- js_graphics/circular_button.js
+++ js_graphics/circular_button.js
@@ -1,26 +1,32 @@
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 
+var btnHov = new Audio("Audio/button_hov.mp3"); // buffers automatically when created
+
+
 canvasHeight = 720;
 canvasWidth = 1280;
 
 function renderButton(x, y, r, text, highlighted) {
 	ctx.beginPath();
 	ctx.arc(x, y, r, 0, 2 * Math.PI);
-	var p = ctx.getImageData(x, y, 1, 1).data; 
-    var hex = "#" + ("000000" + rgbToHex(p[0], p[1], p[2])).slice(-6);
-	ctx.fillStyle = hex;
-	ctx.fill();
+	//var p = ctx.getImageData(x, y, 1, 1).data; 
+  //var hex = "#" + ("000000" + rgbToHex(p[0], p[1], p[2])).slice(-6);
+	//ctx.fillStyle = hex;
+	//ctx.fill();
 	ctx.lineWidth = 3;
 	ctx.strokeStyle = '#69B00C';
   if (highlighted) {
-    ctx.shadowBlur = 10;
+    ctx.lineWidth = 4;
+    ctx.shadowBlur = 15;
     ctx.shadowColor = '#6EB80D';
   } else {
     ctx.shadowBlur = 0;
+    ctx.lineWidth = 3;
   }
-	
 	ctx.stroke();
+
+  
 
 	ctx.font = "18px Calibri";
 	ctx.fillStyle = '#FFFFFF';
@@ -63,8 +69,8 @@ function renderCanvas(circles) {
   ctx.fillStyle = gradient;
   ctx.fillRect(0,0,canvasWidth, canvasHeight);
   
-  renderTestLine();
-
+  //renderTestLine();
+  drawLine(circles[0], circles[1])
   for (var i=0; i < circles.length; i++) {
     renderButton(circles[i].x, circles[i].y, circles[i].r, circles[i].text, circles[i].hl);
   }
@@ -80,7 +86,34 @@ function renderTestLine() {
   ctx.stroke();
 }
 
+function drawLine(c1, c2) {
+  a = Math.atan2(c2.y-c1.y, c2.x-c1.x);
 
+  x0 = c1.x + c1.r*Math.cos(a + 0.5);
+  y0 = c1.y + c1.r*Math.sin(a + 0.5);
+
+  x1 = c1.x + 2*c1.r*Math.cos(a + 0.5);
+  y1 = c1.y + 2*c1.r*Math.sin(a + 0.5);
+
+
+
+  a = a + Math.PI;
+
+  x2 = c2.x + 2*c2.r*Math.cos(a + 0.5);
+  y2 = c2.y + 2*c2.r*Math.sin(a + 0.5);
+
+  x3 = c2.x + c2.r*Math.cos(a + 0.5);
+  y3 = c2.y + c2.r*Math.sin(a + 0.5);
+
+  ctx.beginPath();
+  ctx.moveTo(x0, y0);
+  ctx.bezierCurveTo(x1,y1,x2,y2,x3,y3);
+  ctx.lineWidth = 2;
+  ctx.shadowBlur = 2;
+  ctx.strokeStyle = '#6EB80D';
+  ctx.lineCap = 'round';
+  ctx.stroke();
+}
 
 var circle1 = {x:canvasWidth/2, y:canvasHeight/2 , r:50, text:'Natural numbers', hl:false}
 var circle2 = {x:canvasWidth/2 + 300, y:canvasHeight/2+ 200, r:40, text:'Complex numbers', hl:false}
@@ -103,10 +136,13 @@ canvas.addEventListener('mousemove', function(evt) {
   //renderCanvas(circles)
   var mousePos = getMousePos(canvas, evt);
   for (var i=0; i < circles.length; i++) {
-    rMouseCenter = Math.pow((mousePos.x-circles[i].x),2) + Math.pow((mousePos.y-circles[i].y),2);
-    console.log(rMouseCenter, Math.pow(circles[i].r,2))
-    if (rMouseCenter < Math.pow(circles[i].r,2)) {
-      circles[i].hl = true;
+    rMouseCenter = (mousePos.x-circles[i].x)*(mousePos.x-circles[i].x) + (mousePos.y-circles[i].y)*(mousePos.y-circles[i].y);
+    //console.log(rMouseCenter, Math.pow(circles[i].r,2))
+    if (rMouseCenter < (circles[i].r)*(circles[i].r)) {
+      if (circles[i].hl == false) {
+        btnHov.play();
+        circles[i].hl = true;
+      }
       renderCanvas(circles);
     } else {
       circles[i].hl = false;
