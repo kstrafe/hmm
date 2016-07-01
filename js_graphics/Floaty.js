e37376eb941572086bc6047e631efed60e124049
Bourgond Aries <macocio@gmail.com>
2016-07-01 23:46:28 +0200

    Make floaty float in a wavy pattern

--- js_graphics/Floaty.js
+++ js_graphics/Floaty.js
@@ -4,6 +4,7 @@ function Floaty(x, y, r) {
     this.x = x;
     this.y = y;
     this.r = r;
+    this.angle = 0;
 }
 
 Floaty.prototype.draw = function (context) {
@@ -15,7 +16,7 @@ Floaty.prototype.draw = function (context) {
     context.shadowColor = '#8E8800';
     context.strokeStyle = '#8E8800';
 
-    context.arc(this.x, this.y, this.r, 0, 2 * Math.PI);
+    context.arc(this.x + Math.sin(this.angle) * 30, this.y, this.r, 0, 2 * Math.PI);
 
     context.stroke();
     context.restore();
@@ -23,4 +24,5 @@ Floaty.prototype.draw = function (context) {
 
 Floaty.prototype.move = function () {
     this.y -= 0.3;
+    this.angle += 0.01;
 };
