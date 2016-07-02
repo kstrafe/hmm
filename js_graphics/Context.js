1273dec6dea8b78561ccbd1e3d13e8493f235761
Bourgond Aries <macocio@gmail.com>
2016-07-02 02:49:05 +0200

    Add more methods to Context

--- js_graphics/Context.js
+++ js_graphics/Context.js
@@ -1,6 +1,7 @@
 "use strict";
 
 function Context(canvas) {
+    this.canvas = canvas;
     this.context = canvas.getContext('2d');
     this.xOffset = 0;
     this.yOffset = 0;
@@ -10,9 +11,21 @@ Context.prototype.drawAbsolute = function (drawable) {
     drawable.draw(this.context);
 };
 
-Context.prototype.draw = function (drawable) {
+Context.prototype.clear = function () {
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+};
+
+Context.prototype.draw = function (drawables) {
+    var i = null;
     this.context.save();
     this.context.translate(-this.xOffset, -this.yOffset);
-    this.drawAbsolute(drawable);
+    for (i = 0; i < drawables.length; i += 1) {
+        this.drawAbsolute(drawables[i]);
+    }
     this.context.restore();
+};
+
+Context.prototype.offsetTemporary = function (x, y) {
+    this.xOffset = x;
+    this.yOffset = y;
 };
