02681ffe785218668d7cba3ac66623426ab40cce
Bourgond Aries <macocio@gmail.com>
2016-07-02 20:58:51 +0200

    Precompute the bezier curve instead of doing so in each draw call

--- js_graphics/Curve.js
+++ js_graphics/Curve.js
@@ -1,27 +1,25 @@
 "use strict";
 
 function Curve(x0, y0, r0, x1, y1, r1) {
-    this.curve = {
-        c1: {
+    var c1 = {
             x: x0,
             y: y0,
             r: r0
         },
-        c2: {
+        c2 = {
             x: x1,
             y: y1,
             r: r1
-        }
-    };
+        };
+    this.bezier = this.compute(c1, c2);
 }
 
-Curve.prototype.draw = function (context) {
-
-    var c1x = this.curve.c1.x,
-        c1y = this.curve.c1.y,
-        c2x = this.curve.c2.x,
-        c2y = this.curve.c2.y,
-        dx = c2x - this.curve.c1.x,
+Curve.prototype.compute = function (c1, c2) {
+    var c1x = c1.x,
+        c1y = c1.y,
+        c2x = c2.x,
+        c2y = c2.y,
+        dx = c2x - c1.x,
         dy = c2y - c1y,
         a = null,
         x0 = null,
@@ -33,27 +31,43 @@ Curve.prototype.draw = function (context) {
         x3 = null,
         y3 = null;
 
-    context.save();
 
     a = Math.atan2(dy, dx);
 
-    x0 = c1x + this.curve.c1.r * Math.cos(a + 0.5) + 5 * Math.sign(dx);
-    y0 = c1y + this.curve.c1.r * Math.sin(a + 0.5) + 5 * Math.sign(dy);
+    x0 = c1x + c1.r * Math.cos(a + 0.5) + 5 * Math.sign(dx);
+    y0 = c1y + c1.r * Math.sin(a + 0.5) + 5 * Math.sign(dy);
 
-    x1 = c1x + 2 * this.curve.c1.r * Math.cos(a + 0.5);
-    y1 = c1y + 2 * this.curve.c1.r * Math.sin(a + 0.5);
+    x1 = c1x + 2 * c1.r * Math.cos(a + 0.5);
+    y1 = c1y + 2 * c1.r * Math.sin(a + 0.5);
 
     a = a + Math.PI;
 
-    x2 = c2x + 2 * this.curve.c2.r * Math.cos(a - 0.5);
-    y2 = c2y + 2 * this.curve.c2.r * Math.sin(a - 0.5);
+    x2 = c2x + 2 * c2.r * Math.cos(a - 0.5);
+    y2 = c2y + 2 * c2.r * Math.sin(a - 0.5);
 
-    x3 = c2x + this.curve.c2.r * Math.cos(a - 0.5) - 5 * Math.sign(dx);
-    y3 = c2y + this.curve.c2.r * Math.sin(a - 0.5) - 5 * Math.sign(dy);
+    x3 = c2x + c2.r * Math.cos(a - 0.5) - 5 * Math.sign(dx);
+    y3 = c2y + c2.r * Math.sin(a - 0.5) - 5 * Math.sign(dy);
+
+    return {
+        x0: x0,
+        y0: y0,
+        x1: x1,
+        y1: y1,
+        x2: x2,
+        y2: y2,
+        x3: x3,
+        y3: y3
+    };
+};
+
+Curve.prototype.draw = function (context) {
+
+    var b = this.bezier;
+    context.save();
 
     context.beginPath();
-    context.moveTo(x0, y0);
-    context.bezierCurveTo(x1, y1, x2, y2, x3, y3);
+    context.moveTo(b.x0, b.y0);
+    context.bezierCurveTo(b.x1, b.y1, b.x2, b.y2, b.x3, b.y3);
     context.lineWidth = 2;
     context.shadowBlur = 2.5;
     context.shadowColor = '#4E8800';
