0d842303f96039d081508426eecba37e2fdc9448
Bourgond Aries <macocio@gmail.com>
2016-07-02 23:53:42 +0200

    Add arithmetic and addition

--- js_graphics/Data.js
+++ js_graphics/Data.js
@@ -17,6 +17,22 @@ var all_bubbles = {
             facts: "A statement that is so evident or well-established, that it is accepted without controversy or question. Thus, the axiom can be used as the premise or starting point for further reasoning or arguments"
         },
 
+        arithmetic: {
+            x: -600,
+            y: -950,
+            r: 80,
+            title: "Arithmetic",
+            facts: "Arithmetic or arithmetics (from the Greek ἀριθμός arithmos, \"number\") is the oldest and most elementary branch of mathematics. It consists of the study of numbers, especially the properties of the traditional operations between them—addition, subtraction, multiplication and division. Arithmetic is an elementary part of number theory, and number theory is considered to be one of the top-level divisions of modern mathematics, along with algebra, geometry, and analysis. The terms arithmetic and higher arithmetic were used until the beginning of the 20th century as synonyms for number theory and are sometimes still used to refer to a wider part of number theory."
+        },
+
+        addition: {
+            x: -600,
+            y: -1600,
+            r: 100,
+            title: "Addition",
+            facts: "Addition (often signified by the plus symbol \"+\") is one of the four basic operations of arithmetic, with the others being subtraction, multiplication and division. The addition of two whole numbers is the total amount of those quantities combined. For example, in the picture on the right, there is a combination of three apples and two apples together, making a total of five apples. This observation is equivalent to the mathematical expression \"3 + 2 = 5\" i.e., \"3 add 2 is equal to 5\"."
+        },
+
         equality: {
             x: 600,
             y: -1200,
@@ -60,6 +76,16 @@ var all_bubbles = {
         axiom_to_equality: {
             begin: "axioms",
             end: "equality"
+        },
+
+        axiom_to_arith: {
+            begin: "axioms",
+            end: "arithmetic"
+        },
+
+        arith_to_add: {
+            begin: "arithmetic",
+            end: "addition"
         },
 
         eq_to_trans: {
