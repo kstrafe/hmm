b2ba8d4be67b59f79fa805f46bf7802697d224d8
Bourgond Aries <macocio@gmail.com>
2016-07-05 01:27:44 +0200

    Add a few more nodes

--- js_graphics/Data.js
+++ js_graphics/Data.js
@@ -29,13 +29,12 @@ var all_bubbles = {
             title: "Zoom",
             facts: "You can use the +/- buttons or the scroll wheel to zoom in our out."
         },
-
         mouseless: {
             x: 0,
             y: -4000,
             r: 100,
             title: "Mouseless",
-            facts: "You can select a node if it's centered, press the space bar to open or close the info window."
+            facts: "You can select a node if it's centered, press the space bar to open and any move to close the info window."
         },
         goal: {
             x: 0,
@@ -58,6 +57,13 @@ var all_bubbles = {
             title: "Numbers",
             facts: "Numbers are mathematical objects. A number can represent a quantity. There are many types of numbers. '1' is a number. '9.8' is also a number."
         },
+        number_system: {
+            x: 600,
+            y: -10500,
+            r: 100,
+            title: "Number System",
+            facts: "The decimal number system is made up of ten symbols: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0. The least significant digit is the rightmost digit. This means that 10 is greater than 1. When we compare, we can pad digits so they become the same length. 10 and 1 can be seen as 10 and 01. A zero to the left does not change the number. A zero to the right does change the number."
+        },
         natural_numbers: {
             x: 0,
             y: -12000,
@@ -78,6 +84,34 @@ var all_bubbles = {
             r: 100,
             title: "Subtraction",
             facts: "You can subtract natural numbers. This creates a new number. The symbol for subtracting is '-' (called minus). You put two natural numbers on either side of the minus: 4 - 3. The expression 4 - 3 is the same as the number 1. The expression 10 - 2 is the same as the number 8."
+        },
+        integer_division: {
+            x: -3000,
+            y: -16500,
+            r: 100,
+            title: "Division",
+            facts: "We divide using the '/' symbol. Two integers on either side can create a new number. The left side is the 'numerator', and the right side is the 'denominator'. The expression 6 / 2 is the same as 3, and integer and natural number. The expression -8 / 2 is the same as -(8 / 2), which is the same as - ( 4 ), which is the same as -4, an integer. What does division mean? It means how often can we subtract the denominator from the numerator until it becomes zero? From 6 we can subtract 2, three times: 6 / 2 is the same as 3. The expression 6 - 2 - 2 - 2 becomes 0."
+        },
+        rationals: {
+            x: -1200,
+            y: -17500,
+            r: 100,
+            title: "Rationals",
+            facts: "You have seen integer division. What if the subtraction of the denominator from the numerator doesn't go to 0? Remember that with the expression 6 / 2, we subtract the right side from the left side (of the / sign) until the left side is 0. What if we have something like 5 / 2? Let's try: 5 - 2 = 3. Now we have 3. 3 -  2 = 1. If we subtract 2 once again, we get -1, an integer. So we can not divide the number. 1 is called the remainder. We can create a new type of number. We call this number a 'Rational Number'. It is called rational because it is defined by a ratio of two numbers. The expression 5 / 2 is 2.5 in decimal notation, because the remainder is only half (0.5) that of 2."
+        },
+        remainder: {
+           x: -3000,
+           y: -17000,
+           r: 100,
+           title: "Remainder",
+           facts: "If you divide a number, you subtract the denominator from the numerator until the numerator is zero. Sometimes, the subtraction hops over the zero. In the expression 7 / 3, we first subtract via 7 - 3, which is 4. Then we subtract via 4 - 3, which is 1. Then we subtract via 1 - 3, which is -2. We have hopped over 0. If we want integer division, we use what we call a 'remainder'. We go back and look at the number that was before the jump over 0. This was 1. So we can now say: 7 / 3 is the same as 2, with remainder 1. Why is it 2? Because we could subtract 2 times."
+        },
+        integer_mul: {
+            x: 1400,
+            y: -16900,
+            r: 100,
+            title: "Integer Multiplication",
+            facts: "When multiplying two integers (using the * or x symbol), you will always get a new integer. 4 x 3 is 12. Remember, integers can be negative, like -5. The expression -5 x 2 is -10, which is an integer. When you multiply negative number, you can move them 'out' of the multiplication: -5 x 2 can be written as -(5 x 2). You can now calculate 5 x 2, which is 10. You put 10 where 5 x 2 is: -(10). You remove the parentheses, and get -10."
         },
         integers: {
             x: 0,
@@ -136,6 +170,10 @@ var all_bubbles = {
             begin: "numbers",
             end: "natural_numbers"
         },
+        numbers_to_number_system: {
+            begin: "numbers",
+            end: "number_system"
+        },
         natural_numbers_to_natural_numbers_addition: {
             begin: "natural_numbers",
             end: "natural_numbers_addition"
@@ -147,6 +185,30 @@ var all_bubbles = {
         natural_numbers_to_natural_numbers_subtraction: {
             begin: "natural_numbers",
             end: "natural_numbers_subtraction"
+        },
+        natural_numbers_subtraction_to_integer_division: {
+            begin: "natural_numbers_subtraction",
+            end: "integer_division"
+        },
+        integers_to_integer_division: {
+            begin: "integers",
+            end: "integer_division"
+        },
+        division_to_rationals: {
+            begin: "integer_division",
+            end: "rationals"
+        },
+        integers_to_integer_mul: {
+            begin: "integers",
+            end: "integer_mul"
+        },
+        integers_to_rationals: {
+            begin: "integers",
+            end: "rationals"
+        },
+        division_to_remainder: {
+            begin: "integer_division",
+            end: "remainder"
         },
         natural_numbers_to_integers: {
             begin: "natural_numbers",
