9ed50fb2ee436f862c449f27a01680d78e2754e4
Bourgond Aries <macocio@gmail.com>
2016-07-04 23:12:20 +0200

    Add basic tutorial and an initial graph

--- js_graphics/Data.js
+++ js_graphics/Data.js
@@ -6,113 +6,158 @@ var all_bubbles = {
             y: 0,
             r: 100,
             title: "Click here!",
-            facts: "Welcome to 'Hmm', the sanctuary of knowledge and understanding! To get started, you can drag yourself anywhere using the mouse. The arrow keys can also be used. Click any named bubble (like this one!) to view its info box. Try to follow the connecting lines upwards. The higher you go, the more you've transcended all of mathematics! If you get stuck, try mastering the preliminaries first.\nhjkl can also be used for scrolling. Press 'e' for Dev-view."
+            facts: "Welcome to 'Hmm', the sanctuary of mathematics! To get started, you can drag yourself anywhere using the mouse. The arrow keys, 'wasd', or 'hjkl' can also be used."
+        },
+        music: {
+            x: 0,
+            y: -1000,
+            r: 100,
+            title: "Music",
+            facts: "To change the mute state of music and sounds, press 'm'. You can also click the top-left music button."
+        },
+        fullscreen: {
+            x: 0,
+            y: -2000,
+            r: 100,
+            title: "Fullscreen",
+            facts: "'Hmm' is best enjoyed in full screen. Most browsers support this. Press F11 to enter or exit fullscreen mode."
+        },
+        zoom: {
+            x: 0,
+            y: -3000,
+            r: 100,
+            title: "Zoom",
+            facts: "You can use the +/- buttons or the scroll wheel to zoom in our out."
         },
 
-        axioms: {
-            x: 600,
-            y: -600,
-            r: 60,
-            title: "Axioms",
-            facts: "A statement that is so evident or well-established, that it is accepted without controversy or question. Thus, the axiom can be used as the premise or starting point for further reasoning or arguments"
+        mouseless: {
+            x: 0,
+            y: -4000,
+            r: 100,
+            title: "Mouseless",
+            facts: "You can select a node if it's centered, press the space bar to open or close the info window."
         },
-
-        oneplusone: {
-            x: 825,
-            y: -650,
-            r: 45,
-            title: "1 + 1 = 2",
-            facts: "Considered by many as the most elementary equation. In order to prove this equation we need to have laid down a number of axioms. Peano axioms is examples. Others might looks at this as a definition in itself."
+        goal: {
+            x: 0,
+            y: -5000,
+            r: 100,
+            title: "Goal",
+            facts: "Your goal is to dive deep into the beauty of mathematics, or not. It's up to you, and you alone."
         },
-
-        arithmetic: {
-            x: -600,
-            y: -950,
-            r: 80,
-            title: "Arithmetic",
-            facts: "Arithmetic or arithmetics (from the Greek ἀριθμός arithmos, \"number\") is the oldest and most elementary branch of mathematics. It consists of the study of numbers, especially the properties of the traditional operations between them—addition, subtraction, multiplication and division. Arithmetic is an elementary part of number theory, and number theory is considered to be one of the top-level divisions of modern mathematics, along with algebra, geometry, and analysis. The terms arithmetic and higher arithmetic were used until the beginning of the 20th century as synonyms for number theory and are sometimes still used to refer to a wider part of number theory."
+        devmode: {
+            x: 1500,
+            y: -5000,
+            r: 100,
+            title: "Dev-mode",
+            facts: "A little more information is available by pressing 'e'. This opens up 'Developer mode'. It just shows you your current position and the mouse position."
         },
-
-        addition: {
-            x: -600,
-            y: -1600,
+        numbers: {
+            x: 0,
+            y: -10000,
+            r: 100,
+            title: "Numbers",
+            facts: "Numbers are mathematical objects. A number can represent a quantity. There are many types of numbers. '1' is a number. '9.8' is also a number."
+        },
+        natural_numbers: {
+            x: 0,
+            y: -12000,
+            r: 100,
+            title: "Natural Numbers",
+            facts: "Natural numbers are whole numbers. They do not have a '.' in the number. 10 is natural number. 8932 is a natural number. -3 is not a natural number, because it has a minus sign (-). 5.5 is not a natural number because it contains a dot '.'."
+        },
+        natural_numbers_addition: {
+            x: -3000,
+            y: -12000,
             r: 100,
             title: "Addition",
-            facts: "Addition (often signified by the plus symbol \"+\") is one of the four basic operations of arithmetic, with the others being subtraction, multiplication and division. The addition of two whole numbers is the total amount of those quantities combined. For example, in the picture on the right, there is a combination of three apples and two apples together, making a total of five apples. This observation is equivalent to the mathematical expression \"3 + 2 = 5\" i.e., \"3 add 2 is equal to 5\"."
+            facts: "You can add natural numbers together. This creates a new number. The symbol for adding is '+' (called plus). You put two natural numbers on either side of the plus: 3 + 4. The expression 3 + 4 is the same as the number 7."
         },
-
-        equality: {
-            x: 600,
-            y: -1200,
-            r: 60,
-            title: "Equality",
-            facts: "Equality is a relationship between two quantities or, more generally two mathematical expressions, asserting that the quantities have the same value, or that the expressions represent the same mathematical object. The equality between A and B is written A = B, and pronounced A equals B. The symbol \"=\" is called an \"equals sign\""
+        natural_numbers_subtraction: {
+            x: -3000,
+            y: -14000,
+            r: 100,
+            title: "Subtraction",
+            facts: "You can subtract natural numbers. This creates a new number. The symbol for subtracting is '-' (called minus). You put two natural numbers on either side of the minus: 4 - 3. The expression 4 - 3 is the same as the number 1. The expression 10 - 2 is the same as the number 8."
         },
-
-        equality_reflexivity: {
-            x: 800,
-            y: -1600,
-            r: 60,
-            title: "Reflexivity",
-            facts: "The Reflexive Property states that for every real number x, x = x. This is an axiom of mathematics. It can be explored by testing any number for x. If x is 2, then 2 = 2, which is true. This will work for any number."
-        },
-
-        equality_symmetry: {
-            x: 400,
-            y: -1600,
-            r: 60,
-            title: "Symmetry",
-            facts: "The Symmetric Property states that for all real numbers x and y, if x = y , then y = x."
-        },
-
-        equality_transitive: {
+        integers: {
             x: 0,
-            y: -1600,
-            r: 60,
-            title: "Transitive",
-            facts: "The Transitive Property states that for all real numbers x, y, and z, if x = y, and y = z, then x = z."
-        }
-
+            y: -16000,
+            r: 100,
+            title: "Integers",
+            facts: "Remember from 'Natural Numbers' that -1 is not a natural number, because it has a minus sign in the number. When you subtract: 3 - 4, you will not get a natural number. The expression 3 - 4 evaluates to -1. This is not a natural number. One type of number can create other types of numbers. All natural numbers are also integers, but not all integers are natural numbers."
+        },
+        natural_multiplication: {
+            x: -5000,
+            y: -14000,
+            r: 100,
+            title: "Multiplication",
+            facts: "Multiplication is an operation on two natural numbers. It always generates a new natural number. It can not create a non-natural number. We use the (*) or (x) symbol for multiplication. The expression 5 x 4 is the same as the number 20. The expression can be read as (5 + 5 + 5 + 5). There are 4 fives, which are added together. This means that 3 x 6 is the same as (3 + 3 + 3 + 3 + 3 + 3). This number is 18."
+        },
+        integer_addition: {
+            x: 0,
+            y: -18000,
+            r: 100,
+            title: "Integer Addition",
+            facts: "Adding integers is very similar to natural numbers. 4 and 5 are both natural numbers as well as integer numbers. Adding them gives 9. This is also a natural number and an integer number. Integers can be negative. This means they have a minus sign in the number. The expression 5 + -3 contains two numbers, '5', and '-3'. When you see a '+ -', you can replace the '+ -' by just '-'. You then get '5 - 3', which is subtraction."
+        },
     },
 
     all_curves = {
-        start_to_axioms: {
+
+        start_to_music: {
             begin: "start",
-            end: "axioms"
+            end: "music"
         },
-
-        axiom_to_equality: {
-            begin: "axioms",
-            end: "equality"
+        music_to_fullscreen: {
+            begin: "music",
+            end: "fullscreen"
         },
-
-        axiom_to_arith: {
-            begin: "axioms",
-            end: "arithmetic"
+        fullscreen_to_zoom: {
+            begin: "fullscreen",
+            end: "zoom"
         },
-
-        axiom_to_oneplusone: {
-            begin: "axioms",
-            end: "oneplusone"
+        zoom_to_mouseless: {
+            begin: "zoom",
+            end: "mouseless"
         },
-
-        arith_to_add: {
-            begin: "arithmetic",
-            end: "addition"
+        mouseless_to_goal: {
+            begin: "mouseless",
+            end: "goal"
         },
-
-        eq_to_trans: {
-            begin: "equality",
-            end: "equality_transitive"
+        goal_to_devmode: {
+            begin: "goal",
+            end: "devmode"
         },
-
-        eq_to_ref: {
-            begin: "equality",
-            end: "equality_reflexivity"
+        goal_to_numbers: {
+            begin: "goal",
+            end: "numbers"
         },
-
-        eq_to_sym: {
-            begin: "equality",
-            end: "equality_symmetry"
-        }
+        numbers_to_natural_numbers: {
+            begin: "numbers",
+            end: "natural_numbers"
+        },
+        natural_numbers_to_natural_numbers_addition: {
+            begin: "natural_numbers",
+            end: "natural_numbers_addition"
+        },
+        natural_numbers_addition_to_natural_multiplication: {
+            begin: "natural_numbers_addition",
+            end: "natural_multiplication"
+        },
+        natural_numbers_to_natural_numbers_subtraction: {
+            begin: "natural_numbers",
+            end: "natural_numbers_subtraction"
+        },
+        natural_numbers_to_integers: {
+            begin: "natural_numbers",
+            end: "integers"
+        },
+        natural_numbers_subtraction_to_integers: {
+            begin: "natural_numbers_subtraction",
+            end: "integers"
+        },
+        integers_to_integer_addition: {
+            begin: "integers",
+            end: "integer_addition"
+        },
     };
