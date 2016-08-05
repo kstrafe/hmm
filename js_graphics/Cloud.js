416d422235331f18ce9f2953c830665fa37ced9e
oaramsda <olea.ramsdal@gmail.com>
2016-08-05 22:55:42 +0200

    Some additions to Cloud.js

--- js_graphics/Cloud.js
+++ js_graphics/Cloud.js
@@ -4,8 +4,8 @@
 
 function Cloud() {
     //this.index = index;
-    this.xys = [14311,-28325, 13123,-28277, 12675, -29161, 13000, -30187, 13351, -30293, 15000, -30109, 15175, -29217, 15238, -29455, 14926,-28700, 14890, -28495, 14311,-28325]
-    //this.name = name;
+    this.xys = [14311,-28325, 13123,-28277, 12675, -29161, 13000, -30187, 13351, -30293, 15000, -30109, 15175, -29217, 15238, -29455, 14926,- 28700, 14890, -28495, 14311, -28325]
+    this.name = 'Quantum Mechanics';
     //this.facts = facts;
     //this.highlighted = highlighted;
     this.colors = new Colors();
@@ -15,9 +15,10 @@ function Cloud() {
 }
 
 Cloud.prototype.draw = function (context, zoomIndex) {
-	var n;
+	var n,
+		metrics;
 
-	if (zoomIndex == 0) {
+	if (zoomIndex <= 1) {
 		context.save();
 		context.strokeStyle = this.colors.getHLByName(this.colorName);
 		context.lineWidth = 20;
@@ -32,8 +33,41 @@ Cloud.prototype.draw = function (context, zoomIndex) {
 		context.closePath();
 		context.fill();
 		context.stroke();
+
+		context.font = 500 + "px Calibri";
+		metrics = context.measureText(this.name);
+		context.globalAlpha = 0.75;
+		context.fillStyle = this.colors.getByName('white');
+		context.fillText(this.name, this.getAvgX() - metrics.width/2, this.getAvgY());
+		//console.log(this.getAvgX());
+		//console.log(this.getAvgY());
+
 		context.restore();
 	}
-	
+};
 
+Cloud.prototype.getAvgX = function () {
+	var n,
+		avgX = 0;
+
+	for (n = 0; n < this.xys.length; n += 2) {
+		avgX = avgX + this.xys[n]
+	}
+
+	avgX = 2 * avgX / this.xys.length;
+
+	return avgX;
+};
+
+Cloud.prototype.getAvgY = function () {
+	var n,
+		avgY = 0;
+
+	for (n = 1; n < this.xys.length; n += 2) {
+		avgY = avgY + this.xys[n]
+	}
+
+avgY = 2 * avgY / this.xys.length;
+
+	return avgY;
 };
